3fa666f627e6ccebe687cc32a502fd7430593426
Victor Bello <you@example.com>
2023-12-06 08:28:14 +0100

    @hjxcj

---
+++ src/app/lifcughcgfhj/page.tsx
@@ -0,0 +1,13 @@
+import Body from "../components/body";
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
